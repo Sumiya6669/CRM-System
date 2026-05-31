6f6356f0c2cfe2e6c33ebf5ca544eb28084cc91d
Sumiya6669 <arni5566778899@gmail.com>
2026-05-31 16:52:54 +0500

    Delete src directory

--- src/pages/auth/InviteRegistrationPage.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-import RegisterPage from '@/pages/auth/RegisterPage';
-
-export default function InviteRegistrationPage() {
-  const { token } = useParams();
-
-  return <RegisterPage inviteToken={token} />;
-}
