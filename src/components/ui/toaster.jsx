99c9fb0002cf75ea48fc2ed5d14ecef528d44f88
Sumiya6669 <arni5566778899@gmail.com>
2026-06-01 00:18:17 +0500

    Delete src directory

--- src/components/ui/toaster.jsx
+++
@@ -1,5 +0,0 @@
-import { Toaster as SonnerToaster } from 'sonner';
-
-export const Toaster = () => {
-  return <SonnerToaster richColors closeButton position="top-right" toastOptions={{ duration: 3200 }} />;
-};
